d9aae8c6247240850e6968c3c34b72f7a0651c17
rkdls8268 <rkdls8268@gmail.com>
2023-07-31 12:46:01 +0900

    [FEAT] spring boot setting

---
+++ gainsta/src/main/java/com/example/gainsta/GainstaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gainsta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GainstaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GainstaApplication.class, args);
+    }
+
+}
---
+++ gainsta/src/test/java/com/example/gainsta/GainstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gainsta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainstaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
